d1a531df3ba04871e475830183626c6b1c57bb48
yewhral <zabrzyjewski@gmail.com>
2017-11-09 17:59:23 +0100

    Code organization

--- src/containers/guestsTiles/GuestTiles.js
+++ src/containers/guestsTiles/GuestTiles.js
@@ -4,6 +4,7 @@ import GuestTile from '../../components/guestTile/GuestTile';
 import ScreenTitle from '../../components/screenTitle/ScreenTitle';
 import NavigationLink from '../../components/navigationLink/NavigationLink';
 import { setGuest, setStrength, setSweetness, setCraziness, setFanciness } from '../../redux/actions';
+import guest from '../../data/guests';
 import asianSmall from '../../images/asianSmall.jpg';
 import './guestTiles.css'
 
@@ -21,37 +22,6 @@ class GuestTiles extends React.Component {
         this.props.setFanciness(guest.fancy);
     };
 
-    guest = {
-        Ariel: {
-            name: "Ariel",
-            strong: 1,
-            sweet: 2,
-            crazy: 3,
-            fancy: 4
-        },
-        Clark: {
-            name: "Clark",
-            strong: 4,
-            sweet: 3,
-            crazy: 2,
-            fancy: 1
-        },
-        Simon: {
-            name: "Simon",
-            strong: 1,
-            sweet: 2,
-            crazy: 2,
-            fancy: 1
-        },
-        Lucy: {
-            name: "Lucy",
-            strong: 3,
-            sweet: 3,
-            crazy: 3,
-            fancy: 3
-        }
-    };
-
     render() {
         return (
             <div className="grid-positioning">
@@ -62,22 +32,22 @@ class GuestTiles extends React.Component {
                     <GuestTile
                         guestPhoto={asianSmall}
                         guestName="Ariel"
-                        onClick={() => {this.guestSelection(this.guest.Ariel)}}
+                        onClick={() => {this.guestSelection(guest.Ariel)}}
                     />
                     <GuestTile
                         guestPhoto={asianSmall}
                         guestName="Clark"
-                        onClick={() => {this.guestSelection(this.guest.Clark)}}
+                        onClick={() => {this.guestSelection(guest.Clark)}}
                     />
                     <GuestTile
                      guestPhoto={asianSmall}
                      guestName="Simon"
-                     onClick={() => {this.guestSelection(this.guest.Simon)}}
+                     onClick={() => {this.guestSelection(guest.Simon)}}
                     />
                     <GuestTile
                         guestPhoto={asianSmall}
                         guestName="Lucy"
-                        onClick={() => {this.guestSelection(this.guest.Lucy)}}
+                        onClick={() => {this.guestSelection(guest.Lucy)}}
                     />
                 </div>
                 <NavigationLink
---
+++ src/data/guests.js
@@ -0,0 +1,31 @@
+const guest = {
+    Ariel: {
+        name: "Ariel",
+        strong: 1,
+        sweet: 2,
+        crazy: 3,
+        fancy: 4
+    },
+    Clark: {
+        name: "Clark",
+        strong: 4,
+        sweet: 3,
+        crazy: 2,
+        fancy: 1
+    },
+    Simon: {
+        name: "Simon",
+        strong: 1,
+        sweet: 2,
+        crazy: 2,
+        fancy: 1
+    },
+    Lucy: {
+        name: "Lucy",
+        strong: 3,
+        sweet: 3,
+        crazy: 3,
+        fancy: 3
+    }
+};
+export default guest
