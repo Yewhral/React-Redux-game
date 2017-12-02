08c79d11f12af6093f712b45182dbb49b505cf52
yewhral <zabrzyjewski@gmail.com>
2017-12-02 21:56:38 +0100

    Liquids placed in a glass

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -9,7 +9,11 @@ const Preview = ({
     return (
         <div className="summaryContainer">
             <ScreenTitle title={title} />
-            <div className="glass">
+            <div className="glassContainer">
+                <div className="glass"></div>
+                <div className="liquid alcohol"></div>
+                <div className="liquid softDrink"></div>
+                <div className="liquid syrup"></div>
             </div>
             <NavigationLink
                 linkText = 'Serve your drink!'
