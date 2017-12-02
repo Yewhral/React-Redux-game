8299fe93099734e85e1638720a6082f8f46a0b16
yewhral <zabrzyjewski@gmail.com>
2017-12-02 22:06:39 +0100

    Reasonable inline styling preparations

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -4,16 +4,38 @@ import NavigationLink from '../navigationLink/NavigationLink';
 import "./preview.css";
 
 const Preview = ({
+
     title,
 }) => {
+    const alcoStyle = {
+        background: "blue"
+    };
+    const softStyle = {
+        background: "blue"
+    };
+    const syrupStyle = {
+        background: "blue"
+    };
     return (
         <div className="summaryContainer">
             <ScreenTitle title={title} />
             <div className="glassContainer">
                 <div className="glass"></div>
-                <div className="liquid alcohol"></div>
-                <div className="liquid softDrink"></div>
-                <div className="liquid syrup"></div>
+                <div
+                    className="liquid alcohol"
+                    style={alcoStyle}
+                >
+                </div>
+                <div
+                    className="liquid softDrink"
+                    style={softStyle}>
+                >
+                </div>
+                <div
+                    className="liquid syrup"
+                    style={syrupStyle}
+                >
+                </div>
             </div>
             <NavigationLink
                 linkText = 'Serve your drink!'
