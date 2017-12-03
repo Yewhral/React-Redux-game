183eda149cb70c322804f5b834eb683f401618d9
yewhral <zabrzyjewski@gmail.com>
2017-12-03 14:48:51 +0100

    Changed routing from HashHistory to BrowserHistory

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Switch} from 'react-router-dom';
 import Credits from './views/Credits';
 import DrinkPreparation from './views/DrinkPreparation';
