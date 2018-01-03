198456103255ad9b51b9f83d6c5ee0e74e5a9f0b
yewhral <zabrzyjewski@gmail.com>
2018-01-03 23:25:22 +0100

    Prepared file for guest comments

---
+++ src/data/comments.js
@@ -0,0 +1,123 @@
+const comments = {
+    Lucy: {
+        strong: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        sweet: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        crazy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        fancy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        }
+    },
+    Clark: {
+        strong: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        sweet: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        crazy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        fancy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        }
+    },
+    Simon: {
+        strong: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        sweet: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        crazy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        fancy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        }
+    },
+    Ariel: {
+        strong: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        sweet: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        crazy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        },
+        fancy: {
+            excessive : '',
+            redundant: '',
+            perfect: '',
+            insufficient: '',
+            deficient: ''
+        }
+    }
+};
+export default comments;
