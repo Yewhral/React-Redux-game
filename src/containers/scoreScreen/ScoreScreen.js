1aa7fa2052f6ba02a78dd5a9bc3b355ead19db7d
yewhral <zabrzyjewski@gmail.com>
2018-01-06 18:08:30 +0100

    Introduced working commenting mechanism

--- src/containers/scoreScreen/ScoreScreen.js
+++ src/containers/scoreScreen/ScoreScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ScreenTitle from '../../components/screenTitle/ScreenTitle';
 import GuestPic from '../../components/guestPic/GuestPic';
+import comments from '../../data/comments';
 import './scoreScreen.css'
 
 class ScoreScreen extends React.Component {
@@ -10,16 +11,28 @@ class ScoreScreen extends React.Component {
         this.state = {};
     }
 
-    returnComment(guest, goal, score) {
-        return `${guest} wanted ${goal} and got ${score}`
+    returnComment(guest, parameter, goal, score) {
+        const scoreDifference = score - goal;
+        const param = comments[guest][parameter];
+        if (scoreDifference === 0){
+            return param.perfect;
+        } else if (scoreDifference === 1) {
+            return param.redundant;
+        } else if (scoreDifference === -1) {
+            return param.insufficient;
+        } else if (scoreDifference >= 2) {
+            return param.excessive;
+        } else if (scoreDifference <= -2) {
+            return param.deficient;
+        }
     }
 
     render() {
         const {strong, sweet, crazy, fancy, photo, guest, drinkPower, drinkSweet, drinkCrazy, drinkFancy} = this.props;
-        const strongComment = this.returnComment(guest, strong, drinkPower);
-        const sweetComment = this.returnComment(guest, sweet, drinkSweet);
-        const crazyComment = this.returnComment(guest, crazy, drinkCrazy);
-        const fancyComment = this.returnComment(guest, fancy, drinkFancy);
+        const strongComment = this.returnComment(guest, 'strong', strong, drinkPower);
+        const sweetComment = this.returnComment(guest, 'sweet', sweet, drinkSweet);
+        const crazyComment = this.returnComment(guest, 'crazy', crazy, drinkCrazy);
+        const fancyComment = this.returnComment(guest, 'fancy', fancy, drinkFancy);
         const commentInfo = `${guest} commented your drink: `;
         return (
             <div className="guestWrapper">
